da19240c60fee2305944f06cadecb6137bbffb25
amandeep <samandeepkaue34@gmail.com>
2024-09-06 17:23:15 -0400

    Interfaces methods defined

--- src/main/java/com/technews/repository/Vote.java
+++ src/main/java/com/technews/repository/Vote.java
@@ -1,6 +1,14 @@
 package com.technews.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Vote extends JpaRepository<Vote, Integer> {
+
+    @Query("SELECT count(*) FROM Vote v where v.postId = :id")
+    int countVotesByPostId(@Param("id") Integer Id);
+
 }
